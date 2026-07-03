e13aa807d03c409a3d7cb2d4da289d25d9ca426f
Hassan-Ahuzali <hassan407@proton.me>
2026-07-04 02:48:29 +0300

    Add docstrings to read_file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """Reads a file and prints its content to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
+        """Reads the content of the file."""
         content = f.read()
         print(content)
