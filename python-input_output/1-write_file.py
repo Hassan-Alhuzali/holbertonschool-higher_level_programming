29ab3aac04df8cf96707db16612aafc513aafe8f
Hassan-Ahuzali <hassan407@proton.me>
2026-07-07 23:50:04 +0300

    Return number of characters written by write_file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,10 +5,9 @@ the number of characters written.
 """
 
 
-def write_file(filename="test.py", text="Holberton School is so cool!"):
+def write_file(filename="", text=""):
     """Writes a string to a text file (UTF-8) and returns
     the number of characters written."""
+
     with open("test.py", "w", encoding="UTF-8") as data_is_written:
-        """wright the content of the file"""
-        content = data_is_written.write("test")
-        print(content, end="")
+        return data_is_written.write(text)
