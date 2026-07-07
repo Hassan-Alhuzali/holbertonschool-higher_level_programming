f0be712dee47a9ee87658830ace448ffbcc1b1bd
Hassan-Ahuzali <hassan407@proton.me>
2026-07-07 23:08:24 +0300

    Add Python script to write text to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Writes a string to a text file (UTF-8) and returns
+the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF-8) and returns
+    the number of characters written."""
+    with open("test.py", "w", encoding="UTF-8") as data_is_written:
+        """wright the content of the file"""
+        content = data_is_written.write("test")
+        print(content, end="")
