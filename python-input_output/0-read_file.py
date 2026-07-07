42a47d8c3890d1cd38204eccd10ae7456e0383a4
Hassan-Ahuzali <hassan407@proton.me>
2026-07-07 14:49:03 +0300

    Add module docstring to read_file script

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+Reads a file and prints its content to stdout.
+"""
+
+
 def read_file(filename=""):
     """Reads a file and prints its content to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
